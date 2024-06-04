9b163a6afce0f4374302a1a2c492c518fd3f1c34
Marco Antonio Russo <ocra94@gmail.com>
2024-06-04 21:31:48 +0200

    aggiunti commenti

--- php_03_Marco_Antonio_Russo/Student.php
+++ php_03_Marco_Antonio_Russo/Student.php
@@ -11,18 +11,18 @@ class Student{
         $this->surname = $surname;
         $this->scores = $scores;
         $this->average = $average;
-        $this->getAverage();
+        $this->getAverage();    // aggiorna la proprietà average nel caso sia stato costruito un oggetto con degli scores
     }
 
-    public function pushScore(float ...$score){
+    public function pushScore(float ...$score){ // metodo che riceve un numero di dati non definito che trasforma in un array e lo pusha dentro la proprietà scores
         foreach($score as $singleScore){
             $this->scores[] = $singleScore;
 
         }
-        $this->getAverage();
+        $this->getAverage();    // aggiorna la proprietà average al momento dell'inserimento di ogni nuovo score
     }
 
-    public function getAverage(){
+    public function getAverage(){   // metodo che controlla se la proprietà scores è vuota, altrimenti inserisce il valore della media fra i vari elementi di scores dentro la proprietà average
         if(count($this->scores)){
             $this->average = array_reduce($this->scores, function($carry, $score){
                 return $carry += $score;
--- php_03_Marco_Antonio_Russo/traccia_2.php
+++ php_03_Marco_Antonio_Russo/traccia_2.php
@@ -4,27 +4,40 @@ include("./Student.php");
 $student1 = new Student("Marco", "Russo");
 $student2 = new Student("Mario", "Rossi");
 $student3 = new Student("Miriam", "Bianchi",[2,6,10]);
-$student4 = new Student("Panzerotto", "Panzerottoli");
+$student4 = new Student("Panzerotto", "Panzerottoli",[10]);
 
+// Studente 1 con valori vuoti
+echo "=========================\n";
 echo  "I voti di $student1->name sono i seguenti: ";
 print_r($student1->scores);
 echo "\n";
+echo "=========================";
 
-
+// Studente 2 con valori pushati con il metodo
+echo "=========================";
+echo "\n";
 $student2->pushScore(6.5);
 echo  "I voti di $student2->name sono i seguenti: ";
 print_r($student2->scores);
 echo "\n";
-
-echo "la media dei voti di $student2->name è: ";
+echo "e la media dei suoi voti è: ";
 print_r($student2->average);
 echo "\n";
+echo "=========================";
 
+
+// Studente 3 con valori dichiarati al momento dell'inizializzazione
+echo "=========================";
+echo "\n";
 echo "la media dei voti di $student3->name è: ";
 print_r($student3->average);
 echo "\n";
+echo "=========================";
 
+echo "=========================";
 
+// Studente 4 con valori sia dichiarati al momento dell'inizializzazione sia pushati successivamente
+echo "\n";
 $bestName = $student4->name . " " . $student4->surname;
 $student4->pushScore(10,8,7.2,9.5,10);
 echo  "I voti di $bestName sono i seguenti: ";
@@ -32,3 +45,5 @@ print_r($student4->scores);
 echo "e la media dei suoi voti è: ";
 print_r($student4->average);
 echo "\n";
+echo "=========================";
+
